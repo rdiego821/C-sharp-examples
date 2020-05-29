6c9d42239a0550320828ae0a2de9f5182a7348da
rdiego821 <Diego.Acero@endava.com>
2020-05-29 12:29:27 -0500

    Add chain of responsibility

---
+++ Chain/JefePiso.cs
@@ -0,0 +1,29 @@
+﻿namespace Chain
+{
+    class JefePiso : IHandler
+    {
+
+        private IHandler siguiente = null;
+
+        public JefePiso(IHandler pSiguiente)
+        {
+            siguiente = pSiguiente;
+        }
+        public double CalculaPrecioFinal(int pCantidad, double pPrecio)
+        {
+            System.Console.WriteLine("Con el jefe de Piso");
+
+            double total = pCantidad * pPrecio;
+
+            if (pCantidad > 100 || total > 75000)
+                total = siguiente.CalculaPrecioFinal(pCantidad, pPrecio);
+            else
+            {
+                if (pCantidad > 20)
+                    total *= 0.9;
+            }
+
+            return total;
+        }
+    }
+}
---
+++ Chain/Vendedor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chain
+{
+    class Vendedor : IHandler
+    {
+        private IHandler siguiente = null;
+
+        public Vendedor(IHandler pSiguiente)
+        {
+            siguiente = pSiguiente;
+        }
+        public double CalculaPrecioFinal(int pCantidad, double pPrecio)
+        {
+            Console.WriteLine("Con el vendedor");
+
+            double total = pCantidad * pPrecio;
+
+            if (pCantidad > 20 || total > 20000)
+            {
+                total = siguiente.CalculaPrecioFinal(pCantidad, pPrecio);
+            }
+            else
+            {
+                if (pCantidad > 10)
+                    total *= 0.95;
+            }
+
+            return total;
+        }
+    }
+}
