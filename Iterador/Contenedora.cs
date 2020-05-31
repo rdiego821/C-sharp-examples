a8a338632ad9076fe8cc499fe8645f3c84cffea4
rdiego821 <Diego.Acero@endava.com>
2020-05-31 13:04:21 -0500

    Add Iterator pattern.

---
+++ Iterador/Contenedora.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+
+namespace Iterador
+{
+    internal class Contenedora : IEnumerable
+    {
+        private int[] valores = new int[10];
+
+        public Contenedora()
+        {
+            for (int n = 0; n < 10; n++)
+                valores[n] = n * n;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return (new ContenedorEnum(valores));
+        }
+    }
+
+    class ContenedorEnum : IEnumerator
+    {
+        public int[] arreglo;
+        private int posicion = -1;
+
+        public ContenedorEnum(int[] pArreglo)
+        {
+            arreglo = pArreglo;
+        }
+
+        public bool MoveNext()
+        {
+            posicion++;
+            if (posicion < arreglo.Length)
+                return true;
+            else
+                return false;
+        }
+
+        public void Reset()
+        {
+            posicion = -1;
+        }
+
+        public object Current
+        {
+            get
+            {
+                return arreglo[posicion];
+            }
+        }
+    }
+}
---
+++ Iterador/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Iterador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Contenedora datos = new Contenedora();
+
+            foreach (int valor in datos)
+            {
+                Console.WriteLine(valor);
+            }
+        }
+    }
+}
